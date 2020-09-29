b3ca5440ee30c00566c9dfe460a7ae7c2dce9e65
Daneesh <mustafadaneesh@gmail.com>
2020-09-29 13:15:55 +0530

    [Daneesh] Refactor : Monthly Wage

--- EmpWageComp/EmpWageComp/Program.cs
+++ EmpWageComp/EmpWageComp/Program.cs
@@ -1,32 +1,40 @@
 ﻿using System;
 
-namespace EmpWageUsingSwitch
+namespace MonthlyWage
 {
     class Program
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int overallEmpWage = 0;
+
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                overallEmpWage = overallEmpWage + empWage;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + overallEmpWage);
         }
+
     }
 }
