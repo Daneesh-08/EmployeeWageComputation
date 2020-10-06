b502f5d8eaa96e52301cf22fc917c7ab3adda4e0
Daneesh <mustafadaneesh@gmail.com>
2020-10-06 15:58:58 +0530

    [Daneesh] Refactor : Employee Wage for Multiple Companies

---
+++ EmpWageComp/EmpWageComp/Wage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageComp
+{
+    class Wage
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public static void CompEmpWage(String Company, int EMP_RATE_PER_HOUR, int Totalhrs, int Totaldays)
+        {
+            int totalEmpHrs = 0;
+            int overallWorkingDays = 0;
+            int overallEmpWage = 0;
+            int empHrs;
+            Random random = new Random();
+            while (totalEmpHrs <= Totalhrs && overallWorkingDays <= Totaldays)
+            {
+                overallWorkingDays++;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                overallEmpWage += empHrs * EMP_RATE_PER_HOUR;
+            }
+            Console.WriteLine("Total Employee Wage for " + Company + " : " + overallEmpWage);
+        }
+    }
+}
